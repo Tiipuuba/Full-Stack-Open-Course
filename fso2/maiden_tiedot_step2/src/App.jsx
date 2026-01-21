f37fac3deb4d161749e3a698445096bd8a1b4ba0
Tiipuuba <Brusila.Robert@gmail.com>
2026-01-21 09:12:35 +0200

    Return for exercise 2.19 "Data for countries".

---
+++ fso2/maiden_tiedot_step2/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+const Countries = ({ countries, onClick }) => {
+  console.log(countries);
+  
+
+  if (countries.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  } else if (countries.length > 1) {
+    return (
+      <div>
+        {countries.map(country => 
+          <p key={country.name.common}>{country.name.common} <button type="button" onClick={() => onClick(country.name.common)}>Show</button></p>
+        )}
+      </div>
+    )
+  } else if (countries.length === 1){
+    return (
+      <div>
+        <h1>{countries[0].name.common}</h1>
+        <p>Capital {countries[0].capital}</p>
+        <h1>Languages</h1>
+        <ul>
+          {Object.entries(countries[0].languages).map(([code, name]) => (
+            <li key={code}>{name}</li>
+          ))}
+        </ul>
+        <img src={countries[0].flags.png}  />
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => {
+        setCountries(response.data.filter((country) => country.name.common.toLowerCase().includes(filter.toLowerCase())))
+      })
+  }, [filter])
+
+  const addFilter = ( event ) => {
+    setFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <p>find countries <input value={filter} onChange={addFilter}/></p>
+      <Countries countries={countries} onClick={setFilter}/>
+    </div>
+  )
+
+}
+
+export default App
