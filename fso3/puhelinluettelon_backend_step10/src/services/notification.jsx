83188c86976a36671794ccd10c67d86bcdaafc84
Tiipuuba <Brusila.Robert@gmail.com>
2026-02-06 12:27:54 +0200

    Return for "Phonebook full stack" 3.11.

--- fso3/puhelinluettelon_backend_step10/src/services/notification.jsx
+++
@@ -1,9 +0,0 @@
-const Notification = (props) => {
-    if (props.message === '') { return null }
-
-    if (props.type === 'error') { <div className="notificationError">{props.message}</div> }
-    
-    return <div className="notification">{props.message}</div>
-}
-
-export default Notification
